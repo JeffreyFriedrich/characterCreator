7baf8c088798cc779a716a1b9ef98b0449a23851
powerhungry4 <jefriedrich4@gmail.com>
2018-03-07 12:55:40 -0800

    reformatted class and race

--- resources/d20class.js
+++ resources/d20class.js
@@ -1,8 +1,9 @@
 //Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Wizard
 //class skills list: "appraise", "balance", "bluff", "climb", "concentration", "craft()", "decipherScript", "diplomacy", "disable device", "disguise", "escapeArtist", "forgery", "gatherInformation", "handleAnimal", "heal", "hide", "intimidate", "jump", "knowledge()", "listen", "moveSilently", "openLock", "perform()", "profession()", "ride", "search", "senseMotive", "sleightOfHand", "spellcraft", "spot", "survival", "swim", "tumble", "useMagicDevice", "useRope"
 var d20class = 
-[
-	{
+{
+	Barbarian: 
+		{
 		//name: identifiable class name
 		name: "Barbarian",
 		// BAB: 0 = full, 1 = 3/4, 2 = half
@@ -11,7 +12,7 @@ var d20class =
 		fortSave: 0,
 		refSave: 1,
 		willSave: 1,
-		// hit die: d4, d6, d8, d10, d12; 1st level
+		// hit die: d4, d6, d8, d10, d12; 1st level receives full hit die; afterwards, they roll for it; reroll 1s if merciful
 		hitDie: 12,
 		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 4,
@@ -26,257 +27,166 @@ var d20class =
 		spellStat: "none",
 		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "none"
-	},
-	{
-		//name: identifiable class name
+		},
+	Bard: 
+		{
 		name: "Bard",
-		// BAB: 0 = full, 1 = 3/4, 2 = half
 		BAB: 1,
-		// All saves: 0 = full, 1 = half
 		fortSave: 1,
 		refSave: 0,
 		willSave: 0,
-		// hit die: d4, d6, d8, d10, d12; 1st level
 		hitDie: 6,
-		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 6,
-		// alignment: array of whitelisted alignments; possible alignments = "LG", "NG", "CG", "LN", "N", "CN", "LE", "NE", "CE"  
 		alignment: ["NG", "CG", "N", "CN", "NE", "CE"],
-		// classSkills: skills that allow full ranks (up to (lvl + 3)) at cost of 1; all other skills go up to ((lvl + 3) / 2) and cost 2 (unless character has Able Learner feat); some nonclass skills cannot be used/invested in untrained
 		classSkills: ["appraise", "balance", "bluff", "climb", "concentration", "craft()", "decipherScript", "diplomacy", "disguise", "escapeArtist", "gatherInformation", "hide", "jump", "knowledge()", "listen", "moveSilently", "perform()", "profession()","senseMotive", "sleightOfHand", "spellcraft", "swim", "tumble", "useMagicDevice"],
-		// All proficiencies: represent a selection of weapons and armor a character can equip; simple and martial weapon proficiency are most common for weapons; light, medium, heavy, and shields are most common for armor
 		weaponProficiencies: ["simple", "bard"],
 		armorProficiencies: ["light", "shields"],
-		// spellStat: what ability modifier impacts spells for this class; CHA, WIS, and INT are common; many classes will have "none"
 		spellStat: "CHA",
-		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "arcane"
-	},
-	{
-		//name: identifiable class name
+		},
+	Cleric: 
+		{
 		name: "Cleric",
-		// BAB: 0 = full, 1 = 3/4, 2 = half
 		BAB: 1,
-		// All saves: 0 = full, 1 = half
 		fortSave: 0,
 		refSave: 1,
 		willSave: 0,
-		// hit die: d4, d6, d8, d10, d12; 1st level
 		hitDie: 8,
-		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 2,
-		// alignment: array of whitelisted alignments; possible alignments = LG, NG, CG, LN, N, CN, LE, NE, CE  
-		//NOTE: cleric alignment must be within 1 level of his deity
 		alignment: ["LG", "NG", "CG", "LN", "N", "CN", "LE", "NE", "CE"],
-		// classSkills: skills that allow full ranks (up to (lvl + 3)) at cost of 1; all other skills go up to ((lvl + 3) / 2) and cost 2 (unless character has Able Learner feat); some nonclass skills cannot be used/invested in untrained
 		//NOTE: clerics may have additional class skills based on domains
 		classSkills: ["concentration", "craft()", "diplomacy", "heal", "knowledge(arcana)", "knowledge(history)", "knowledge(religion)", "knowledge(planes)", "profession()", "spellcraft"],
-		// All proficiencies: represent a selection of weapons and armor a character can equip; simple and martial weapon proficiency are most common for weapons; light, medium, heavy, and shields are most common for armor
 		weaponProficiencies: ["simple"],
 		armorProficiencies: ["light", "medium", "heavy", "shields"],
-		// spellStat: what ability modifier impacts spells for this class; CHA, WIS, and INT are common; many classes will have "none"
 		spellStat: "WIS",
-		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "divine"
-	},
-	{
-		//name: identifiable class name
+		},
+	Druid: 
+		{
 		name: "Druid",
-		// BAB: 0 = full, 1 = 3/4, 2 = half
 		BAB: 1,
-		// All saves: 0 = full, 1 = half
 		fortSave: 0,
 		refSave: 1,
 		willSave: 0,
-		// hit die: d4, d6, d8, d10, d12; 1st level
 		hitDie: 8,
-		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 4,
-		// alignment: array of whitelisted alignments; possible alignments = LG, NG, CG, LN, N, CN, LE, NE, CE  
 		alignment: ["NG", "LN", "N", "CN", "NE"],
-		// classSkills: skills that allow full ranks (up to (lvl + 3)) at cost of 1; all other skills go up to ((lvl + 3) / 2) and cost 2 (unless character has Able Learner feat); some nonclass skills cannot be used/invested in untrained
 		classSkills: ["concentration", "craft()", "diplomacy", "handleAnimal", "heal", "knowledge(nature)", "listen", "profession()", "ride", "spellcraft", "spot", "survival", "swim"],
-		// All proficiencies: represent a selection of weapons and armor a character can equip; simple and martial weapon proficiency are most common for weapons; light, medium, heavy, and shields are most common for armor
 		weaponProficiencies: ["druid"],
 		armorProficiencies: ["druid"],
-		// spellStat: what ability modifier impacts spells for this class; CHA, WIS, and INT are common; many classes will have "none"
 		spellStat: "WIS",
-		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "divine"
-	},
-	{
-		//name: identifiable class name
+		},
+	Fighter: 
+		{
 		name: "Fighter",
-		// BAB: 0 = full, 1 = 3/4, 2 = half
 		BAB: 0,
-		// All saves: 0 = full, 1 = half
 		fortSave: 0,
 		refSave: 1,
 		willSave: 1,
-		// hit die: d4, d6, d8, d10, d12; 1st level
 		hitDie: 10,
-		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 2,
-		// alignment: array of whitelisted alignments; possible alignments = LG, NG, CG, LN, N, CN, LE, NE, CE  
 		alignment: ["LG", "NG", "CG", "LN", "N", "CN", "LE", "NE", "CE"],
-		// classSkills: skills that allow full ranks (up to (lvl + 3)) at cost of 1; all other skills go up to ((lvl + 3) / 2) and cost 2 (unless character has Able Learner feat); some nonclass skills cannot be used/invested in untrained
 		classSkills: ["climb", "craft", "handleAnimal", "intimidate", "jump", "ride", "swim"],
-		// All proficiencies: represent a selection of weapons and armor a character can equip; simple and martial weapon proficiency are most common for weapons; light, medium, heavy, and shields are most common for armor
 		weaponProficiencies: ["simple", "martial"],
 		armorProficiencies: ["light", "medium", "heavy", "shields", "towerShields"],
-		// spellStat: what ability modifier impacts spells for this class; CHA, WIS, and INT are common; many classes will have "none"
 		spellStat: "none",
-		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "none"
-	},
-	{
-		//name: identifiable class name
+		},
+	Monk: 
+		{
 		name: "Monk",
-		// BAB: 0 = full, 1 = 3/4, 2 = half
 		BAB: 1,
-		// All saves: 0 = full, 1 = half
 		fortSave: 0,
 		refSave: 0,
 		willSave: 0,
-		// hit die: d4, d6, d8, d10, d12; 1st level
 		hitDie: 8,
-		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 4,
-		// alignment: array of whitelisted alignments; possible alignments = LG, NG, CG, LN, N, CN, LE, NE, CE  
 		alignment: ["LG", "LN", "LE"],
-		// classSkills: skills that allow full ranks (up to (lvl + 3)) at cost of 1; all other skills go up to ((lvl + 3) / 2) and cost 2 (unless character has Able Learner feat); some nonclass skills cannot be used/invested in untrained
 		classSkills: ["balance", "climb", "concentration", "craft()", "diplomacy", "escapeArtist", "hide", "jump", "knowledge(arcana)", "knowledge(religion)", "listen", "moveSilently", "perform()", "profession()", "senseMotive", "spot", "swim", "tumble"],
-		// All proficiencies: represent a selection of weapons and armor a character can equip; simple and martial weapon proficiency are most common for weapons; light, medium, heavy, and shields are most common for armor
 		weaponProficiencies: ["monk"],
 		armorProficiencies: ["none"],
-		// spellStat: what ability modifier impacts spells for this class; CHA, WIS, and INT are common; many classes will have "none"
 		spellStat: "none",
-		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "none"
-	},
-	{
-		//name: identifiable class name
+		},
+	Paladin: 
+		{
 		name: "Paladin",
-		// BAB: 0 = full, 1 = 3/4, 2 = half
 		BAB: 0,
-		// All saves: 0 = full, 1 = half
 		fortSave: 0,
 		refSave: 1,
 		willSave: 1,
-		// hit die: d4, d6, d8, d10, d12; 1st level
 		hitDie: 10,
-		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 2,
-		// alignment: array of whitelisted alignments; possible alignments = LG, NG, CG, LN, N, CN, LE, NE, CE  
 		alignment: ["LG"],
-		// classSkills: skills that allow full ranks (up to (lvl + 3)) at cost of 1; all other skills go up to ((lvl + 3) / 2) and cost 2 (unless character has Able Learner feat); some nonclass skills cannot be used/invested in untrained
 		classSkills: ["concentration", "craft()", "diplomacy", "handleAnimal", "heal", "knowledge(nobilityAndRoyalty)", "knowledge(religion)", "profession()", "ride", "senseMotive"],
-		// All proficiencies: represent a selection of weapons and armor a character can equip; simple and martial weapon proficiency are most common for weapons; light, medium, heavy, and shields are most common for armor
 		weaponProficiencies: ["simple", "martial"],
 		armorProficiencies: ["light", "medium", "heavy", "shields"],
-		// spellStat: what ability modifier impacts spells for this class; CHA, WIS, and INT are common; many classes will have "none"
 		spellStat: "WIS",
-		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "divine"
-	},
-	{
-		//name: identifiable class name
+		},
+	Ranger: 
+		{
 		name: "Ranger",
-		// BAB: 0 = full, 1 = 3/4, 2 = half
 		BAB: 0,
-		// All saves: 0 = full, 1 = half
 		fortSave: 0,
 		refSave: 0,
 		willSave: 1,
-		// hit die: d4, d6, d8, d10, d12; 1st level
 		hitDie: 8,
-		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 6,
-		// alignment: array of whitelisted alignments; possible alignments = LG, NG, CG, LN, N, CN, LE, NE, CE  
 		alignment: ["LG", "NG", "CG", "LN", "N", "CN", "LE", "NE", "CE"],
-		// classSkills: skills that allow full ranks (up to (lvl + 3)) at cost of 1; all other skills go up to ((lvl + 3) / 2) and cost 2 (unless character has Able Learner feat); some nonclass skills cannot be used/invested in untrained
 		classSkills: ["climb", "concentration", "craft()", "handleAnimal", "heal", "hide", "jump", "knowledge(dungeoneering)", "knowledge(geography)", "knowledge(nature)", "listen", "moveSilently", "profession()", "ride", "search", "spot", "survival", "swim", "useRope"],
-		// All proficiencies: represent a selection of weapons and armor a character can equip; simple and martial weapon proficiency are most common for weapons; light, medium, heavy, and shields are most common for armor
 		weaponProficiencies: ["simple", "martial"],
 		armorProficiencies: ["light", "shields"],
-		// spellStat: what ability modifier impacts spells for this class; CHA, WIS, and INT are common; many classes will have "none"
 		spellStat: "WIS",
-		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "divine"
-	},
-	{
-		//name: identifiable class name
+		},
+	Rogue: 
+		{
 		name: "Rogue",
-		// BAB: 0 = full, 1 = 3/4, 2 = half
 		BAB: 1,
-		// All saves: 0 = full, 1 = half
 		fortSave: 1,
 		refSave: 0,
 		willSave: 1,
-		// hit die: d4, d6, d8, d10, d12; 1st level
 		hitDie: 6,
-		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 8,
-		// alignment: array of whitelisted alignments; possible alignments = LG, NG, CG, LN, N, CN, LE, NE, CE  
 		alignment: ["LG", "NG", "CG", "LN", "N", "CN", "LE", "NE", "CE"],
-		// classSkills: skills that allow full ranks (up to (lvl + 3)) at cost of 1; all other skills go up to ((lvl + 3) / 2) and cost 2 (unless character has Able Learner feat); some nonclass skills cannot be used/invested in untrained
 		classSkills: ["appraise", "balance", "bluff", "climb", "craft()", "decipherScript", "diplomacy", "disableDevice", "disguise", "escapeArtist", "forgery", "gatherInformation", "hide", "intimidate", "jump", "knowledge (local)", "listen", "moveSilently", "openLock", "perform()", "profession", "search", "senseMotive", "sleightOfHand", "spot", "swim", "tumble", "useMagicDevice", "useRope"],
-		// All proficiencies: represent a selection of weapons and armor a character can equip; simple and martial weapon proficiency are most common for weapons; light, medium, heavy, and shields are most common for armor
 		weaponProficiencies: ["simple", "rogue"],
 		armorProficiencies: ["light"],
-		// spellStat: what ability modifier impacts spells for this class; CHA, WIS, and INT are common; many classes will have "none"
 		spellStat: "none",
-		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "none"
-	},
-	{
-		//name: identifiable class name
+		},
+	Sorceror: 
+		{
 		name: "Sorceror",
-		// BAB: 0 = full, 1 = 3/4, 2 = half
 		BAB: 2,
-		// All saves: 0 = full, 1 = half
 		fortSave: 1,
 		refSave: 1,
 		willSave: 0,
-		// hit die: d4, d6, d8, d10, d12; 1st level
 		hitDie: 4,
-		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 2,
-		// alignment: array of whitelisted alignments; possible alignments = LG, NG, CG, LN, N, CN, LE, NE, CE  
 		alignment: ["LG", "NG", "CG", "LN", "N", "CN", "LE", "NE", "CE"],
-		// classSkills: skills that allow full ranks (up to (lvl + 3)) at cost of 1; all other skills go up to ((lvl + 3) / 2) and cost 2 (unless character has Able Learner feat); some nonclass skills cannot be used/invested in untrained
 		classSkills: ["bluff", "concentration", "craft()", "knowledge(arcana)", "profession()", "spellcraft"],
-		// All proficiencies: represent a selection of weapons and armor a character can equip; simple and martial weapon proficiency are most common for weapons; light, medium, heavy, and shields are most common for armor
 		weaponProficiencies: ["simple"],
 		armorProficiencies: ["none"],
-		// spellStat: what ability modifier impacts spells for this class; CHA, WIS, and INT are common; many classes will have "none"
 		spellStat: "CHA",
-		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "arcane"
-	},
-	{
-		//name: identifiable class name
+		},
+	Wizard: 
+		{
 		name: "Wizard",
-		// BAB: 0 = full, 1 = 3/4, 2 = half
 		BAB: 2,
-		// All saves: 0 = full, 1 = half
 		fortSave: 1,
 		refSave: 1,
 		willSave: 0,
-		// hit die: d4, d6, d8, d10, d12; 1st level
 		hitDie: 4,
-		// skill points: class-specific-number; calculation is (CSN + INT modifier + (isHuman)) * (lvl + 3)
 		skillPoints: 2,
-		// alignment: array of whitelisted alignments; possible alignments = LG, NG, CG, LN, N, CN, LE, NE, CE  
 		alignment: ["LG", "NG", "CG", "LN", "N", "CN", "LE", "NE", "CE"],
-		// classSkills: skills that allow full ranks (up to (lvl + 3)) at cost of 1; all other skills go up to ((lvl + 3) / 2) and cost 2 (unless character has Able Learner feat); some nonclass skills cannot be used/invested in untrained
 		classSkills: ["concentration", "craft()", "decipherScript", "knowledge()", "profession()", "spellcraft"],
-		// All proficiencies: represent a selection of weapons and armor a character can equip; simple and martial weapon proficiency are most common for weapons; light, medium, heavy, and shields are most common for armor
 		weaponProficiencies: ["wizard"],
 		armorProficiencies: ["none"],
-		// spellStat: what ability modifier impacts spells for this class; CHA, WIS, and INT are common; many classes will have "none"
 		spellStat: "INT",
-		// spellType: what kind of spells do they produce; common types are arcane and divine; many classes will have none and few will have both 
 		spellType: "arcane"
-	},
-]
+		},
+}
--- resources/race.js
+++ resources/race.js
@@ -1,21 +1,55 @@
 var race = 
-[
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-]
+{
+	human: 
+		{
+		//name: as found in the literature
+		name:"Human",
+		//abilityChanges: changes made 
+		abilityChanges: {},
+		// size: 0 = fine, 1 = diminuitive, 2 = tiny, 3 = small, 4 = medium, 5 = large, 6 = huge, 7 = gargantuan, 8 = colossal; attack and AC formula = 2 ^ (3 - size) (for size < 4) or -(2) ^ (size - 5) (for size > 4) or 0 (size = 4); special attacks modifier (bull rush, grapple, etc) = (size - 4) * 4; hide modifier = (4 - size) * 4
+		size: 4,
+		// speed: ft moved in a move action
+		speed: 30,
+		//vision: 0 = normal, 1 = low-light vision, 2 = darkvision, 3 = low-light vision and darkvision
+		vision: 0,
+		//visionRange: range at which creatures can see, according to their vision, before taking penalties; creatures without extra vision abilities will have a 0
+		visionRange: 0,
+		//weaponFamiliarity: exotic weapons that a race treats as martial (a fighter of this race can use this weapon without taking a feat or penalty)
+		weaponFamiliarity: [],
+		//weaponProficiency: weapons that all members of this race have proficiency with
+		weaponProficiency: [],
+		//bonusFeatures: extra features, often conditional, that a race has outside current categories;
+		bonusFeatures: ["bonus feat at 1st level", "4 extra skill points at first level, 1 at additional levels"],
+		//racialBonus: racial features that apply unconditionally to certain categories; often skills
+		racialBonus: [],
+		//blood: is cosidered this/these race(s) for certain effects; often written as "___ blood"; if blank, refer to race name
+		blood: [],
+		// automaticLanguages: languages always known by that race
+		automaticLanguages: ["common"],
+		// bonusLanguages: languages added to the list a character can know at lvl 1 (number of these known determined from INT mod)
+		//NOTE: humans access to any bonus language does not include secret languages like druidic
+		bonusLanguages: ["any"],
+		//spell-like abilities: race specific abilities that can be cast under certain conditions
+		spellLikeAbilities: [],
+		// favoredClass: multiclass character do not take an experience point penalty for their favored class levels (when this rule is used)
+		favoredClass: "any"
+		},
+	dwarf: 
+		{
+		name:"Dwarf", 
+		abilityChanges: {CON: 2, CHA: -2},
+		size: 4,
+		speed: 20,
+		vision: 2,
+		visionRange: 60,
+		weaponFamiliarity: ["dwarven waraxe", "dwarven urgrosh"],
+		weaponProficiency: [],
+		bonusFeatures: [],
+		racialBonus: [],
+		blood: [],
+		automaticLanguages: ["common"],
+		bonusLanguages: ["any"],
+		spellLikeAbilities: [],
+		favoredClass: "any"
+		}
+}
