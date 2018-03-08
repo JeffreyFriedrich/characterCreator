7d93610bbe5ec9a52dc6f8b4ac6169569f655c18
powerhungry4 <jefriedrich4@gmail.com>
2018-03-08 13:08:44 -0800

    minor chracterMaster changes

--- resources/characterMaster.js
+++ resources/characterMaster.js
@@ -2,8 +2,20 @@ const d20class = require('./d20class.js');
 const race = require('./race.js');
 const deity = require('./deity.js');
 
-var characterMaster = {
-    name: ''
+var characterMaster = 
+{
+    characterName: '',
+    playerName: '',
+    d20class: {},
+    ecl: null,
+    race: '',
+    size: null,
+    gender: '',
+    alignment: '',
+    deity: '',
+    height: '',
+    weight: '',
+    looks: ''
 }
 
 module.exports.default =
