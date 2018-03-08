0c32db9cb6c76f3d0813d82bd8d72bb15f60a7ea
gregory prouty <gprout009@gmail.com>
2018-03-07 21:18:35 -0800

    added characterMaster

---
+++ resources/characterMaster.js
@@ -0,0 +1,11 @@
+const d20class = require('./d20class.js');
+const race = require('./race.js');
+const deity = require('./deity.js');
+
+var characterMaster = {
+    characterName: ''
+}
+
+export default {
+    characterMaster
+}
